5bc11bb782324aecf1b2654fddce6be24fcb7bec
Mohammadreza Ghorbandoost <rezadev20@gmail.com>
2023-07-06 12:56:09 +0330

    Repository initialization

---
+++ PortfolioMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PortfolioMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
