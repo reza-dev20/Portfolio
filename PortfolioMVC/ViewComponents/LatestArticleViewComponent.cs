d6466960b4061b72879d548f3dfe223726e3f2de
Mohammadreza Ghorbandoost <rezadev20@gmail.com>
2023-08-10 16:18:47 +0330

    use VIewComponents for Projects and Articles

---
+++ PortfolioMVC/ViewComponents/LatestArticleViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PortfolioMVC.Models;
+
+namespace PortfolioMVC.ViewComponents
+{
+	public class LatestArticleViewComponent : ViewComponent
+	{
+		public IViewComponentResult Invoke() 
+		{
+			var Articles = new List<Article>() 
+			{
+				new Article(1,"Angular","دوره حرفه ای صفر تا صد Angular","blog-post-thumb-card-1.jpg"),
+				new Article(2,"برنامه نویسی چیست ؟‌ ","همه چیز هایی که باید بدانید + کاربردها","blog-post-thumb-card-2.jpg"),
+				new Article(3,"صفحه نمایش لپ تاپ","معرفی انواع صفحه نمایش لپ تاپ","blog-post-thumb-card-3.jpg"),
+				new Article(4,"جاوااسکریپت","دوره آموزش جاوااسکریپت","blog-post-thumb-card-4.jpg")
+			};
+			return View("_LatestArticle",Articles);
+		}
+	}
+}
---
+++ PortfolioMVC/ViewComponents/ProjectsViewComponent.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PortfolioMVC.Models;
+
+namespace PortfolioMVC.ViewComponents
+{
+	public class ProjectsViewComponent : ViewComponent
+	{
+		public IViewComponentResult Invoke() {
+			List<Project> projects = new List<Project>() 
+			{
+				new Project(1,"صرافی دیجیتال","خرید و فروش ارز دیجیتال...","Ali","project-1.jpg"),
+				new Project(2,"Tracer","آنالیز داده های ورودی و خروجی...","Hasan","project-2.jpg"),
+				new Project(1,"SEO","Search Engine Optimize...","Karim","project-3.jpg"),
+				new Project(1,"تدرس خصوصی","آموزش زبان های برنامه نویسی","Sara","project-4.jpg")
+			};
+			return View("_Projects",projects);
+		}
+	}
+}
